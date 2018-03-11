04bd9442804c6f5ee1cd81c373e3e083816587a6
athukuribhavani <bhavani.athukuri85@gmail.com>
2018-03-11 18:51:07 +0530

    first com mit

---
+++ src/main/java/sample/sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("nbfb");
+		System.out.println("bhavani bhavani  bhavani");
+	}
+
+}
